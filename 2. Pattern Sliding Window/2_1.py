093bd5d890daaa4029ea8fe2ae9dd0d6bc82169c
sarveshwarans <34870770+sarveshwarans@users.noreply.github.com>
2022-05-24 10:10:32 -0700

    [2.2, 2.8, 2.4, 3.6, 2.8]

--- 2. Pattern Sliding Window/2_1.py
+++ 2. Pattern Sliding Window/2_1.py
@@ -1,4 +1,15 @@
 def findAverageOfKInInputArray(sizeK,inputArray):
+    windowStart = 0
+    windowEnd = 0
+    windowSum = 0
+    outputList = []
+    for windowEnd in range(len(inputArray)):
+        windowSum += inputArray[windowEnd]
+        if windowEnd>=sizeK-1:
+            outputList.append(windowSum/sizeK)
+            windowSum-=inputArray[windowStart]
+            windowStart+=1
+    return outputList
 
 
 def main():
