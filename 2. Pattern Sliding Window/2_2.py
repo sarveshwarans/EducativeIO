2a112ce8f660cb53e2c4e76b5f9e48fb6beb3b80
sarveshwarans <34870770+sarveshwarans@users.noreply.github.com>
2022-05-24 10:28:20 -0700

    9

--- 2. Pattern Sliding Window/2_2.py
+++ 2. Pattern Sliding Window/2_2.py
@@ -1,15 +1,15 @@
 def findMaximumSumSubArrayOfSizeK(sizeK, inputArray):
-    maxValueOutput = 0
-    windowStart = 0 
+    windowStart = 0
     windowEnd = 0
     windowSum = 0
+    maxWindowSum = 0
     for windowEnd in range(len(inputArray)):
-        windowSum += inputArray[windowEnd]
-        if windowEnd >= sizeK-1 :
-            maxValueOutput = max(maxValueOutput,windowSum)
-            windowSum -= inputArray[windowStart]
-            windowStart += 1
-    return maxValueOutput
+        windowSum+=inputArray[windowEnd]
+        if windowEnd>=sizeK-1:
+            maxWindowSum = max(maxWindowSum,windowSum)
+            windowSum-=inputArray[windowStart]
+            windowStart+=1
+    return maxWindowSum
 
 def main():
     result = findMaximumSumSubArrayOfSizeK(3,[2,1,5,1,3,2])
