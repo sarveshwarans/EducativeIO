7a26705a1829af37705ee427c0df4afbea3b1a95
sarveshwarans <34870770+sarveshwarans@users.noreply.github.com>
2022-05-25 10:26:46 -0700

    3-1

--- 3. Pattern Two Pointers/3_1.py
+++ 3. Pattern Two Pointers/3_1.py
@@ -14,8 +14,24 @@ def pairWithTargetSum(target,inputList):
         elif currentSum>target:
             rightPointer-=1
 
+def pairWithTargetSumUsingHM(target,inputList):
+    hashMap ={}
+    for currentIndex,currentNumber in enumerate(inputList):
+        requiredNumber = target-currentNumber
+        if requiredNumber not in hashMap:
+            hashMap[currentNumber]=currentIndex
+        else:
+            return [currentIndex,hashMap[requiredNumber]]
+        
+
 def main():
     print(pairWithTargetSum(6,[1, 2, 3, 4, 6]))
     print(pairWithTargetSum(11,[2, 5, 9, 11]))  
+    print(pairWithTargetSumUsingHM(6,[1, 2, 3, 4, 6]))
+    print(pairWithTargetSumUsingHM(11,[2, 5, 9, 11]))  
+
 main()
-         
+
+
+
+
