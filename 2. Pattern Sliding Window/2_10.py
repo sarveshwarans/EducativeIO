11ef461ba89adb4e52f4543a2c6d42dc96fbfe60
Sarveshwaran Sampathkumar <sarveshswaran@gmail.com>
2022-05-24 17:33:57 -0700

    12 234

--- 2. Pattern Sliding Window/2_10.py
+++ 2. Pattern Sliding Window/2_10.py
@@ -1,5 +1,27 @@
 def stringAnagrams(patternString, inputString):
-
+    windowStart = 0
+    windowEnd = 0
+    patternFreq = {}
+    for character in patternString:
+        if character not in patternFreq:
+            patternFreq[character]=0
+        patternFreq[character]+=1
+    windowFreq = {}
+    outputIndicesList = []
+    for windowEnd in range(len(inputString)):
+        currentChar=inputString[windowEnd]
+        if currentChar not in windowFreq:
+            windowFreq[currentChar]=0
+        windowFreq[currentChar]+=1
+        if windowEnd>=len(patternString)-1:
+            if windowFreq==patternFreq:
+                outputIndicesList.append(windowStart)
+            leftChar=inputString[windowStart]
+            windowFreq[leftChar]-=1
+            if windowFreq[leftChar]==0:
+                del windowFreq[leftChar]
+            windowStart+=1
+    return outputIndicesList
 
 def main():
     print(stringAnagrams("pq","ppqp"))
