54fd26622d0ac50cefd4ce9e6e5827ae1f76afb0
sarveshwarans <34870770+sarveshwarans@users.noreply.github.com>
2022-05-26 12:14:55 -0700

    3.4

--- 3. Pattern Two Pointers/3_4.py
+++ 3. Pattern Two Pointers/3_4.py
@@ -6,11 +6,11 @@ def searchTriplet(inputList):
     #If i am processing 3, then i would need -3(target sum) to get a 0
     inputList.sort()
     outputTriplet=[]
-    for i in range(len(inputList)-1):
+    for i in range(len(inputList)):
         currentNumber = inputList[i]
         targetSum = -currentNumber
         leftPointer = i+1
-        if i>0 and inputList[i]==inputList[i+1]:
+        if i>0 and inputList[i]==inputList[i-1]:
             continue
         else:
             findTriplet(inputList,targetSum,leftPointer,outputTriplet)
@@ -25,6 +25,13 @@ def findTriplet(inputList,targetSum,leftPointer,outputTriplet):
             outputTriplet.append([-targetSum,inputList[leftPointer],inputList[rightPointer]])
             leftPointer+=1 #I am doing this because there are cases where we can find two pairs for a target sum
             rightPointer-=1 #If we dont do this then we will miss out on one pair
+            #Now that we increased LP and decreased RP, we need to figure out if the new LP and RP are different
+            #We need to compare with the old P. 
+            #For LP, the old will be LP-1 and for RP, the old will be RP+1
+            while leftPointer<rightPointer and inputList[leftPointer]==inputList[leftPointer-1]:
+                leftPointer+=1
+            while leftPointer<rightPointer and inputList[rightPointer]==inputList[rightPointer+1]:
+                rightPointer-=1
         elif currentSum>targetSum:
             rightPointer-=1
         else:
@@ -34,4 +41,6 @@ def findTriplet(inputList,targetSum,leftPointer,outputTriplet):
 def main():
     print(searchTriplet([-3, 0, 1, 2, -1, 1, -2]))
     print(searchTriplet([-5, 2, -1, -2, 3]))  
+    print(searchTriplet([-1,0,1,2,-1,-4]))  
+    print(searchTriplet([-2,0,0,2,2])) 
 main()
