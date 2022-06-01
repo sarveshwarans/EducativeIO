64e2c93ad3314bf71212dacf099b2bae263af4f7
sarveshwarans <34870770+sarveshwarans@users.noreply.github.com>
2022-06-01 01:14:49 -0700

    2.8

--- 2. Pattern Sliding Window/2_8.py
+++ 2. Pattern Sliding Window/2_8.py
@@ -1,4 +1,4 @@
-def findLongestSubarryWithOnesAfterReplacement(kSize,inputArray):
+def findLongestSubarryWithOnesAfterReplacement1(kSize,inputArray):
     windowStart = 0
     windowEnd = 0
     numberFrequence = {}
@@ -19,6 +19,27 @@ def findLongestSubarryWithOnesAfterReplacement(kSize,inputArray):
             windowStart += 1
         maxLongestSubArrayCount = max(maxLongestSubArrayCount, windowEnd-windowStart+1)
     return maxLongestSubArrayCount
+
+
+def findLongestSubarryWithOnesAfterReplacement(kSize,inputArray):
+    start=0
+    onesLengthAfterReplacement=0
+    numberFreq={}
+    for end in range(len(inputArray)):
+        currentNumber=inputArray[end]
+        if currentNumber not in numberFreq:
+            numberFreq[currentNumber]=0
+        numberFreq[currentNumber]+=1
+        if numberFreq[0]>kSize:
+            leftMostNumber=inputArray[start]
+            numberFreq[leftMostNumber]-=1
+            if numberFreq[leftMostNumber]==0:
+                del numberFreq[leftMostNumber]
+            start+=1
+        currentLength=end-start+1
+        onesLengthAfterReplacement=max(onesLengthAfterReplacement,currentLength)
+    return onesLengthAfterReplacement
+
 
 def main():
     print(findLongestSubarryWithOnesAfterReplacement(2,[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1]))
